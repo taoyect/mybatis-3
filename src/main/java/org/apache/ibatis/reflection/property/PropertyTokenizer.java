475f19ec7528e8cdb60effeedbd9a0e63510e5c1
taoye <596296093@qq.com>
2023-08-13 19:08:39 +0800

    属性分词器学习

--- src/main/java/org/apache/ibatis/reflection/property/PropertyTokenizer.java
+++ src/main/java/org/apache/ibatis/reflection/property/PropertyTokenizer.java
@@ -19,11 +19,46 @@ import java.util.Iterator;
 
 /**
  * @author Clinton Begin
+ *
+ * 属性分词器
+ *
+ *  fullname: "name"
+ *    name:"name"
+ *    indexedName:"name"
+ *    index:null
+ *    children:null
+ *
+ *  fullname: "student.name"
+ *    name:"student"
+ *    indexedName:"student"
+ *    index:null
+ *    children:"name"
+ *
+ *  fullname: "students[0]"
+ *    name:"students"
+ *    indexedName:"students[0]"
+ *    index:"0"
+ *    children:null
+ *
+ *  fullname: "claim.owners[0].ownerName"
+ *  PropertyTokenizer first = new PropertyTokenizer(fullname)
+ *    name:"claim"
+ *    indexedName:"claim"
+ *    index:null
+ *    children:"owners[0].ownerName"
+ *  PropertyTokenizer second = new PropertyTokenizer(children)
+ *    name:"owners"
+ *    indexedName:"owners[0]"
+ *    index:"0"
+ *    children:"ownerName"
+ *
  */
 public class PropertyTokenizer implements Iterator<PropertyTokenizer> {
-  private String name;
+  private String name;  //父表达式，一般对应被操作Bean的fieldName,根据该name可以获取被操作bean的属性的getter/setter
+  //带索引的表达式，由父表达式和下标组成，indexedName一般应用于带下标的表达式。一些bean的属性类型可能是array|list，
+  // 对于array|list，我们必须知道下标才能操作其中的元素，indexedName负责保存这类属性的字段名称和下标
   private final String indexedName;
-  private String index;
+  private String index; //下标索引
   private final String children;
 
   public PropertyTokenizer(String fullname) {
